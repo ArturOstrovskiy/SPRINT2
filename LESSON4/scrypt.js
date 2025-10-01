1fb87721e6ec9fcc10f7afba64df69fda60462d1
Artur Ostrovskiy <artur_ostrovskiy@mail.ru>
2025-10-01 17:51:07 +0300

    commit

---
+++ LESSON4/scrypt.js
@@ -0,0 +1,25 @@
+let gold = 100;
+let buildingName = "House";
+let buildingCoast = 64;
+
+console.log(`Amount: ${gold}`);
+if (gold >= buildingCoast) {
+gold -= buildingCoast;
+console.log(`${buildingName} completed`);
+	console.log(`Amount: ${gold}`);
+} else {
+	console.log("Not completed");
+	console.log(`Amount: ${gold}`);
+}
+
+buildingName = "Garage";
+buildingCoast = 16;
+
+if (gold >= buildingCoast) {
+	gold -= buildingCoast;
+	console.log(`${buildingName} completed`);
+	console.log(`Amount: ${gold}`);
+} else {
+	console.log("Not completed");
+	console.log(`Amount: ${gold}`);
+}
