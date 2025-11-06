d65686792946ff662918cb78230f9dd8222428de
Artur Ostrovskiy <artur_ostrovskiy@mail.ru>
2025-11-07 00:46:46 +0300

    commit

--- LESSON8/script.js
+++ LESSON8/script.js
@@ -14,9 +14,12 @@ const model = {
 	},
 	render(){
 		//1. Remove previous view
-		console.clear();
+		// console.clear();
+		document.body.innerHTML = ``
 		//2. Create new view
 		console.log(this.courseData);
+		const container = Course(this.courseData)
+		document.body.append(container);
 	},
 	createLesson(lessonTitle){
 		//1. Change DATA
@@ -52,8 +55,39 @@ const model = {
 		model.render();
 	}
 }
-
 function init(){
 	model.render();
 }
 init();
+
+function Course(data) {
+	const container = document.createElement("div");
+	//заголовок
+	const title = document.createElement("h1");
+	title.setAttribute("id", "course-title");
+	title.classList.add("title");
+	title.textContent = data.title;
+	//список
+	const list = document.createElement("ul");
+	title.classList.add("list");
+  //элементы списка
+	const lessons = data.lessons
+	for (let i = 0; i < lessons.length; i++) {
+		const lesson = lessons[i];
+		const listItem = document.createElement("li");
+		listItem.classList.add("item");
+		listItem.textContent = lesson.title;
+		if (lesson.isDone === true) {
+			listItem.classList.add("done");
+		}
+		listItem.textContent = lesson.title;
+	const deleteBtn = document.createElement("button");
+		deleteBtn.textContent = 'x'
+	list.append(deleteBtn);
+	list.append(listItem);
+	}
+
+	container.append(title,list);
+	return container;
+
+}
